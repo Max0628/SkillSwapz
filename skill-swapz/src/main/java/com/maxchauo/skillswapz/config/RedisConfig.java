e221c4fbaa58583d56d3e880e4b778db795fb721
Maxchauo0628 <maxchauo0628@gmail.com>
2024-10-09 21:00:27 +0800

    [bug] edit redis config

--- skill-swapz/src/main/java/com/maxchauo/skillswapz/config/RedisConfig.java
+++ skill-swapz/src/main/java/com/maxchauo/skillswapz/config/RedisConfig.java
@@ -3,33 +3,53 @@ package com.maxchauo.skillswapz.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.text.SimpleDateFormat;
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
 
-    // 从环境变量或配置文件中读取 Redis 密码
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
     @Value("${spring.data.redis.password}")
     private String redisPassword;
 
+    @Value("${spring.data.redis.ssl.enabled}")
+    private boolean sslEnabled;
+
+    @Value("${spring.data.redis.timeout}")
+    private long timeout;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
-        lettuceConnectionFactory.setPassword(redisPassword); // 使用从环境变量中读取的密码
-        return lettuceConnectionFactory;
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(redisHost);
+        redisConfig.setPort(redisPort);
+        redisConfig.setPassword(redisPassword);
+
+        LettuceClientConfiguration.LettuceClientConfigurationBuilder builder = LettuceClientConfiguration.builder();
+        builder.useSsl().disablePeerVerification();
+        builder.commandTimeout(Duration.ofMillis(timeout));
+
+        return new LettuceConnectionFactory(redisConfig, builder.build());
     }
 
     @Bean
@@ -40,10 +60,8 @@ public class RedisConfig {
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
-
         return template;
     }
-
 
     @Bean
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
@@ -72,19 +90,9 @@ public class RedisConfig {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
-
-        // 注册 JavaTimeModule 支持 Java 8 时间类型
         objectMapper.registerModule(new JavaTimeModule());
-
-        // 设置日期格式，按照你需要的格式，例如 "yyyy-MM-dd HH:mm:ss"
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
-
-        try {
-            return objectMapper;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+        return objectMapper;
     }
 }
